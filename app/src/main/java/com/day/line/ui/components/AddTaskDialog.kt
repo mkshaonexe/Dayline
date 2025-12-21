97ae90bb508afe94edda0733051021507a11b5c8
MK X Shaon <mkshaonnew31@gmail.com>
2025-12-21 15:23:01 +0600

    Fix missing imports in AddTaskDialog

--- app/src/main/java/com/day/line/ui/components/AddTaskDialog.kt
+++ app/src/main/java/com/day/line/ui/components/AddTaskDialog.kt
@@ -2,11 +2,14 @@ package com.day.line.ui.components
 
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Close
